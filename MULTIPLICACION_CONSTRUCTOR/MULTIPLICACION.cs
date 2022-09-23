fdb2b46e5eb6233006678eef1968329355807d19
dk188 <dk188@LAPTOP-UVIC0HB6>
2022-09-23 02:40:11 -0600

    Agregar archivos de proyecto.

---
+++ MULTIPLICACION_CONSTRUCTOR/MULTIPLICACION.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MULTIPLICACION_CONSTRUCTOR
+{
+   
+        internal class clmultiplicacion
+        {
+            //campos, propiedades, como es 
+            private float n1, n2;
+            //constructor
+            public clmultiplicacion(float n1, float n2)
+            {
+                this.n1 = n1;
+                this.n2 = n2;
+            }
+           
+            public float multiplicar()
+            {
+                return (n1 * n2);
+            }
+           
+        }
+}
+
